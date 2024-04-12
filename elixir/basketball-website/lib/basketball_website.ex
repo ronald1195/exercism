ccf2cf40c2606b328b144ed08b4af0c9f284c651
Ronald Munoz <ronald1195@icloud.com>
2024-04-12 17:20:38 -0600

    basketball-website solution

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,22 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    path_array = String.split(path, ".")
+    do_extract_from_path(data, path_array)
+  end
+
+  defp do_extract_from_path(data, []) do
+    data
+  end
+
+  defp do_extract_from_path(data, [hd | rest]) do
+    case data[hd] do
+      nil -> nil
+      _ -> do_extract_from_path(data[hd], rest)
+    end
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    path_array = String.split(path, ".")
+    get_in(data, path_array)
   end
 end
