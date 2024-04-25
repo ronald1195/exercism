101cda98c5941b34f2ae987b2a85717601452679
Ronald Munoz <ronald1195@icloud.com>
2024-04-24 23:42:43 -0600

    partial solution

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,10 +1,29 @@
 defmodule FileSniffer do
+
+  @extension_media_types %{
+    "exe" => "application/octet-stream",
+    "bmp" => "image/bmp",
+    "png" => "image/png",
+    "jpg" => "image/jpg",
+    "gif" => "image/gif"
+  }
+
+  @signature_media_types %{
+    "\x7F\x45" => "application/octet-stream",
+    "\x42\x4D" => "image/bmp",
+    "\x89\x50" => "image/png",
+    "\xFF\xD8" => "image/jpg",
+    "\x47\x49" => "image/gif"
+  }
+
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    @extension_media_types[extension]
   end
 
-  def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+  def type_from_binary(<<signature::binary-size(8), _::binary>>) do
+    # @signature_media_types[signature]
+    IO.inspect(signature, label: "signature value:::")
+    IO.inspect(@signature_media_types[signature], label: "signature:::")
   end
 
   def verify(file_binary, extension) do
