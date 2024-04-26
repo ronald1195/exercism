ff9e8cb5f0744b342a48b65a2d071ae8ad327a9e
Ronald Munoz <ronald1195@icloud.com>
2024-04-26 16:59:52 -0600

    file_sniffer solution

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,5 +1,4 @@
 defmodule FileSniffer do
-
   @extension_media_types %{
     "exe" => "application/octet-stream",
     "bmp" => "image/bmp",
@@ -8,25 +7,31 @@ defmodule FileSniffer do
     "gif" => "image/gif"
   }
 
-  @signature_media_types %{
-    "\x7F\x45" => "application/octet-stream",
-    "\x42\x4D" => "image/bmp",
-    "\x89\x50" => "image/png",
-    "\xFF\xD8" => "image/jpg",
-    "\x47\x49" => "image/gif"
-  }
-
   def type_from_extension(extension) do
     @extension_media_types[extension]
   end
 
-  def type_from_binary(<<signature::binary-size(8), _::binary>>) do
-    # @signature_media_types[signature]
-    IO.inspect(signature, label: "signature value:::")
-    IO.inspect(@signature_media_types[signature], label: "signature:::")
-  end
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>), do: "application/octet-stream"
+  def type_from_binary(<<0x42, 0x4D, _::binary>>), do: "image/bmp"
+
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>),
+    do: "image/png"
+
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>), do: "image/jpg"
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>), do: "image/gif"
+  def type_from_binary(_), do: nil
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    case type_from_binary(file_binary) do
+      nil -> {:error, "Warning, file format and file extension do not match."}
+      binary_type -> verify_extension_and_binary_type(extension, binary_type)
+    end
+  end
+
+  defp verify_extension_and_binary_type(extension, binary_type) do
+    case type_from_extension(extension) do
+      extension_type when extension_type == binary_type -> {:ok, extension_type}
+      _ -> {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
