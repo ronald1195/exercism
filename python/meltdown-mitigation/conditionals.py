89aff40b79b7c80286559e50fa57a67dd945a589
Ronald Munoz <ronald1195@icloud.com>
2024-04-29 23:33:01 -0600

    solution

--- python/meltdown-mitigation/conditionals.py
+++ python/meltdown-mitigation/conditionals.py
@@ -46,12 +46,14 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
     if efficiency >= 80:
         return "green"
-    elif efficiency >= 60:
+
+    if efficiency >= 60:
         return "orange"
-    elif efficiency >= 30:
+
+    if efficiency >= 30:
         return "red"
-    else:
-        return "black"
+
+    return "black"
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
@@ -67,4 +69,12 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
 
-    pass
+    production = temperature * neutrons_produced_per_second
+
+    if production < 0.9 * threshold:
+        return "LOW"
+
+    if 0.9 * threshold <= production <= 1.1 * threshold:
+        return "NORMAL"
+
+    return "DANGER"
