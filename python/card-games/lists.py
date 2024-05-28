fdf144fddb4a3b0e5d44c85cccde088dcbf5a7da
Ronald Munoz <ronald1195@icloud.com>
2024-05-27 23:04:05 -0600

    solution

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -11,7 +11,7 @@ def get_rounds(number):
     :return: list - current round and the two that follow.
     """
 
-    pass
+    return [number, number + 1, number + 2]
 
 
 def concatenate_rounds(rounds_1, rounds_2):
@@ -22,7 +22,7 @@ def concatenate_rounds(rounds_1, rounds_2):
     :return: list - all rounds played.
     """
 
-    pass
+    return rounds_1 + rounds_2
 
 
 def list_contains_round(rounds, number):
@@ -33,7 +33,7 @@ def list_contains_round(rounds, number):
     :return: bool - was the round played?
     """
 
-    pass
+    return number in rounds
 
 
 def card_average(hand):
@@ -43,7 +43,7 @@ def card_average(hand):
     :return: float - average value of the cards in the hand.
     """
 
-    pass
+    return sum(hand) / len(hand)
 
 
 def approx_average_is_average(hand):
@@ -53,7 +53,9 @@ def approx_average_is_average(hand):
     :return: bool - does one of the approximate averages equal the `true average`?
     """
 
-    pass
+    return (hand[0] + hand[-1]) / 2 == card_average(hand) or hand[
+        len(hand) // 2
+    ] == card_average(hand)
 
 
 def average_even_is_average_odd(hand):
@@ -63,7 +65,7 @@ def average_even_is_average_odd(hand):
     :return: bool - are even and odd averages equal?
     """
 
-    pass
+    return sum(hand[::2]) / len(hand[::2]) == sum(hand[1::2]) / len(hand[1::2])
 
 
 def maybe_double_last(hand):
@@ -73,4 +75,7 @@ def maybe_double_last(hand):
     :return: list - hand with Jacks (if present) value doubled.
     """
 
-    pass
+    if hand[-1] == 11:
+        hand[-1] *= 2
+
+    return hand
