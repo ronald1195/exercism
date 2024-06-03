ec3a812ea981e46b2f3c13dc29042b220b3891af
Ronald Munoz <ronald1195@icloud.com>
2024-06-02 22:18:50 -0600

    solution

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -4,23 +4,17 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-    diff = List.myers_difference(a, b)
-
-    case diff do
-      [] ->
-        :equal
-
-      _ ->
-        {additions, deletions} = Enum.reduce(diff, {0, 0}, fn
-          {:add, _}, {adds, dels} -> {adds + 1, dels}
-          {:delete, _}, {adds, dels} -> {adds, dels + 1}
-        end)
-
-        cond do
-          additions == 0 -> :sublist
-          deletions == 0 -> :superlist
-          true -> :unequal
-        end
+    cond do
+      equals?(a, b) -> :equal
+      sublist?(a, b) -> :superlist
+      sublist?(b, a) -> :sublist
+      true -> :unequal
     end
+  end
+
+  defp equals?(a, b), do: a === b
+  defp sublist?([], _), do: false
+  defp sublist?(l1 = [_|t], l2) do
+    List.starts_with?(l1, l2) or sublist?(t, l2)
   end
 end
