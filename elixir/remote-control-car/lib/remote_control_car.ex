3d4c46b5b054c9cbc998fb6bfc043f1e294b3e16
Ronald Munoz <ronald1195@icloud.com>
2024-08-13 23:38:46 -0600

    partial

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,35 @@
 defmodule RemoteControlCar do
   # Please implement the struct with the specified fields
-
+  @enforce_keys [:nickname]
+  defstruct [:nickname, :battery_percentage, :distance_driven_in_meters]
   def new() do
     # Please implement the new/0 function
+    %RemoteControlCar{nickname: "none", battery_percentage: 100, distance_driven_in_meters: 0}
   end
 
   def new(nickname) do
     # Please implement the new/1 function
+    %RemoteControlCar{nickname: nickname, battery_percentage: 100, distance_driven_in_meters: 0}
   end
 
-  def display_distance(remote_car) do
+  def display_distance(%RemoteControlCar{} = remote_car) do
     # Please implement the display_distance/1 function
+    "#{remote_car.distance_driven_in_meters} meters"
   end
 
-  def display_battery(remote_car) do
+  def display_battery(%RemoteControlCar{} = remote_car) do
     # Please implement the display_battery/1 function
+    case remote_car.battery_percentage do
+      0 -> "Battery empty"
+      _ -> "Battery at #{remote_car.battery_percentage}%"
+    end
   end
 
-  def drive(remote_car) do
+  def drive(%RemoteControlCar{} = remote_car) do
     # Please implement the drive/1 function
+    case remote_car.battery_percentage do
+      0 -> remote_car
+      _ -> %RemoteControlCar{remote_car | distance_driven_in_meters: remote_car.distance_driven_in_meters + 20, battery_percentage: remote_car.battery_percentage - 1}
+    end
   end
 end
