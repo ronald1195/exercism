b4d7fd2981f523afe2befb2982816e83ce699422
Ronald Munoz <ronald1195@icloud.com>
2024-08-14 23:42:37 -0600

    solution

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,9 +1,10 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
-    # Please implement the get_combinations/3 function
     maximum_price = Keyword.get(options, :maximum_price, 100.00)
 
-    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] and top[:price] + bottom[:price] < maximum_price do
+    for top <- tops,
+        bottom <- bottoms,
+        top[:base_color] != bottom[:base_color] and top[:price] + bottom[:price] < maximum_price do
       {top, bottom}
     end
   end
