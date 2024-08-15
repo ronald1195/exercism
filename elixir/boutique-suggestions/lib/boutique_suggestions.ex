df598bdad76b4465a41ef57709392638f91c96fb
Ronald Munoz <ronald1195@icloud.com>
2024-08-14 23:34:38 -0600

    partial

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,8 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
+  def get_combinations(tops, bottoms, options \\ []) do
     # Please implement the get_combinations/3 function
+    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] and top[:price] + bottom[:price] < options[:maximum_price] do
+      {top, bottom}
+    end
   end
 end
