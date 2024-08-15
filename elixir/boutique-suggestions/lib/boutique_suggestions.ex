3210781ff67ceba44be207b105535d8ddd449479
Ronald Munoz <ronald1195@icloud.com>
2024-08-14 23:40:29 -0600

    partial

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,7 +1,9 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
     # Please implement the get_combinations/3 function
-    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] and top[:price] + bottom[:price] < options[:maximum_price] do
+    maximum_price = Keyword.get(options, :maximum_price, 100.00)
+
+    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] and top[:price] + bottom[:price] < maximum_price do
       {top, bottom}
     end
   end
