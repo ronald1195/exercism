e70e2289b1fe7ba2b5bcaf3707862de46f45f28f
Ronald Munoz <ronald1195@icloud.com>
2024-08-20 00:11:52 -0600

    partial

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -7,15 +7,25 @@ end
 defmodule CommunityGarden do
   def start(opts \\ []) do
     # Please implement the start/1 function
-    Agent.start(fn -> %{} end, opts)
+    Agent.start(fn -> [] end, opts)
   end
 
   def list_registrations(pid) do
     # Please implement the list_registrations/1 function
+    Agent.get(pid, fn state -> state end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    Agent.get_and_update(pid, fn state ->
+      plot_id =
+        case state do
+          [] -> 1
+          _ -> Enum.max_by(state, & &1.plot_id).plot_id + 1
+        end
+      plot = %Plot{plot_id: plot_id, registered_to: register_to}
+      new_state = [plot | state]
+      {plot, new_state}
+    end)
   end
 
   def release(pid, plot_id) do
