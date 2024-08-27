669f2e28c74fec7054d300460e1f78c3f5c59801
Ronald Munoz <ronald1195@icloud.com>
2024-08-26 23:10:08 -0600

    solution

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -1,4 +1,4 @@
-# Use the Plot struct as it is provided
+# Use th Plot struct as it is provided
 defmodule Plot do
   @enforce_keys [:plot_id, :registered_to]
   defstruct [:plot_id, :registered_to]
@@ -6,13 +6,10 @@ end
 
 defmodule CommunityGarden do
   def start(opts \\ []) do
-    # Please implement the start/1 function
-    initial_state = %{plots: [], next_id: 1}
-    Agent.start(fn -> initial_state end, opts)
+    Agent.start(fn -> %{plots: [], next_id: 1} end, opts)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
     Agent.get(pid, fn state -> state.plots end)
   end
 
@@ -26,7 +23,6 @@ defmodule CommunityGarden do
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
     case list_registrations(pid) do
       [] ->
         {:error, "No registrations found"}
@@ -36,7 +32,7 @@ defmodule CommunityGarden do
           nil ->
             {:error, "Plot not found"}
 
-          plot ->
+          _ ->
             Agent.update(pid, fn state ->
               new_plots = Enum.reject(state.plots, fn p -> p.plot_id == plot_id end)
               %{state | plots: new_plots}
@@ -48,7 +44,6 @@ defmodule CommunityGarden do
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
     case list_registrations(pid) do
       [] ->
         {:not_found, "plot is unregistered"}
