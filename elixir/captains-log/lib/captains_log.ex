45be695dab31d643da95d3c1441873a8c7e2528e
Ronald Munoz <ronald1195@icloud.com>
2024-09-20 00:57:42 -0600

    partial

--- elixir/captains-log/lib/captains_log.ex
+++ elixir/captains-log/lib/captains_log.ex
@@ -2,16 +2,21 @@ defmodule CaptainsLog do
   @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
-    # partial
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    "NCC-" <> Integer.to_string(Enum.random(1000..9999))
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    initial_star_date = Enum.random(41000..42000)
+
+    if initial_star_date < 41999 do
+      initial_star_date + :rand.uniform()
+    else
+      initial_star_date
+    end
   end
 
   def format_stardate(stardate) do
