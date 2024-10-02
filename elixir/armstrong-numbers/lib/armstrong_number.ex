c36823fead952165bd10dccf309bfb7033526a9a
Ronald Munoz <ronald1195@icloud.com>
2024-10-01 23:47:53 -0600

    partial

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -5,5 +5,24 @@ defmodule ArmstrongNumber do
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
+    number
+    |> number_of_digits()
+    |> sum_power(number)
+    |> Kernel.==(number)
+  end
+
+  def number_of_digits(number) do
+    number
+    |> Integer.to_string()
+    |> String.length()
+  end
+
+  def sum_power(power, number) do
+    number
+    |> Integer.to_string()
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn x -> x ** power end)
+    |> Enum.sum()
   end
 end
