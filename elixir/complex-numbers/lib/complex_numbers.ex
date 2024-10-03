31fac06feb0752acd895365e80afac42d8678f98
Ronald Munoz <ronald1195@icloud.com>
2024-10-02 18:28:09 -0600

    partial

---
+++ elixir/complex-numbers/lib/complex_numbers.ex
@@ -0,0 +1,76 @@
+defmodule ComplexNumbers do
+  @typedoc """
+  In this module, complex numbers are represented as a tuple-pair containing the real and
+  imaginary parts.
+  For example, the real number `1` is `{1, 0}`, the imaginary number `i` is `{0, 1}` and
+  the complex number `4+3i` is `{4, 3}'.
+  """
+  @type complex :: {number, number}
+
+  @doc """
+  Return the real part of a complex number
+  """
+  @spec real(a :: complex) :: number
+  def real(a) do
+    elem(a, 0)
+  end
+
+  @doc """
+  Return the imaginary part of a complex number
+  """
+  @spec imaginary(a :: complex) :: number
+  def imaginary(a) do
+    elem(a, 1)
+  end
+
+  @doc """
+  Multiply two complex numbers, or a real and a complex number
+  """
+  @spec mul(a :: complex | number, b :: complex | number) :: complex
+  def mul(a, b) do
+    {real(a) * real(b) - imaginary(a) * imaginary(b),
+     real(a) * imaginary(b) + real(b) * imaginary(a)}
+  end
+
+  @doc """
+  Add two complex numbers, or a real and a complex number
+  """
+  @spec add(a :: complex | number, b :: complex | number) :: complex
+  def add(a, b) do
+  end
+
+  @doc """
+  Subtract two complex numbers, or a real and a complex number
+  """
+  @spec sub(a :: complex | number, b :: complex | number) :: complex
+  def sub(a, b) do
+  end
+
+  @doc """
+  Divide two complex numbers, or a real and a complex number
+  """
+  @spec div(a :: complex | number, b :: complex | number) :: complex
+  def div(a, b) do
+  end
+
+  @doc """
+  Absolute value of a complex number
+  """
+  @spec abs(a :: complex) :: number
+  def abs(a) do
+  end
+
+  @doc """
+  Conjugate of a complex number
+  """
+  @spec conjugate(a :: complex) :: complex
+  def conjugate(a) do
+  end
+
+  @doc """
+  Exponential of a complex number
+  """
+  @spec exp(a :: complex) :: complex
+  def exp(a) do
+  end
+end
