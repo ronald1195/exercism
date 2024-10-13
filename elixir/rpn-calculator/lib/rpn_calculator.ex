e9c521ba087836085040a95bf33893e6d977fe0f
Ronald Munoz <ronald1195@icloud.com>
2024-10-12 18:29:21 -0600

    solution

--- elixir/rpn-calculator/lib/rpn_calculator.ex
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,21 @@
 defmodule RPNCalculator do
   def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      RuntimeError -> :error
+    end
   end
 
   def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      exception -> {:error, exception.message}
+    end
   end
 end
