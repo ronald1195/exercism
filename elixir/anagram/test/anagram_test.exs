9f164eef7c398389a66255e940e2fa1b30f2aa44
Ronald Munoz <ronald1195@icloud.com>
2024-11-08 22:51:24 -0700

    partial

--- elixir/anagram/test/anagram_test.exs
+++ elixir/anagram/test/anagram_test.exs
@@ -19,61 +19,61 @@ defmodule AnagramTest do
     assert matches == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "detects anagram" do
     matches = Anagram.match("listen", ~w(enlists google inlets banana))
     assert matches == ~w(inlets)
   end
 
-  @tag :pending
+  # @tag :pending
   test "detects three anagrams" do
     matches = Anagram.match("allergy", ~w(gallery ballerina regally clergy largely leading))
     assert matches == ~w(gallery regally largely)
   end
 
-  @tag :pending
+  # @tag :pending
   test "detects multiple anagrams with different case" do
     matches = Anagram.match("nose", ~w(Eons ONES))
     assert matches == ~w(Eons ONES)
   end
 
-  @tag :pending
+  # @tag :pending
   test "does not detect non-anagrams with identical checksum" do
     matches = Anagram.match("mass", ~w(last))
     assert matches == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "detect anagrams case-insensitively" do
     matches = Anagram.match("orchestra", ~w(cashregister Carthorse radishes))
     assert matches == ~w(Carthorse)
   end
 
-  @tag :pending
+  # @tag :pending
   test "detects anagrams using case-insensitive subject" do
     matches = Anagram.match("Orchestra", ~w(cashregister carthorse radishes))
     assert matches == ~w(carthorse)
   end
 
-  @tag :pending
+  # @tag :pending
   test "detects anagrams using case-insensitive possible matches" do
     matches = Anagram.match("orchestra", ~w(cashregister Carthorse radishes))
     assert matches == ~w(Carthorse)
   end
 
-  @tag :pending
+  # @tag :pending
   test "does not detect an anagram if the original word is repeated" do
     matches = Anagram.match("go", ~w(goGoGO))
     assert matches == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "anagrams must use all letters exactly once" do
     matches = Anagram.match("tapper", ~w(patter))
     assert matches == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "words are not anagrams of themselves" do
     matches = Anagram.match("BANANA", ~w(BANANA))
     assert matches == []
