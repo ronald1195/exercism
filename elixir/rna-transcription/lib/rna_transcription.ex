05581231c74d27c9b41c618d376807851c6bb532
Ronald Munoz <ronald1195@icloud.com>
2024-12-30 15:28:47 -0700

    solution

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -9,5 +9,14 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna
+    |> Enum.map(&transcribe_nucleotide/1)
   end
+
+  @spec transcribe_nucleotide(char) :: char
+  defp transcribe_nucleotide(?G), do: ?C
+  defp transcribe_nucleotide(?C), do: ?G
+  defp transcribe_nucleotide(?T), do: ?A
+  defp transcribe_nucleotide(?A), do: ?U
+  defp transcribe_nucleotide(char), do: raise(ArgumentError, "Invalid nucleotide: #{char}")
 end
--- elixir/rna-transcription/test/rna_transcription_test.exs
+++ elixir/rna-transcription/test/rna_transcription_test.exs
@@ -6,27 +6,27 @@ defmodule RnaTranscriptionTest do
     assert RnaTranscription.to_rna(~c"") == ~c""
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes guanine to cytosine" do
     assert RnaTranscription.to_rna(~c"G") == ~c"C"
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes cytosine to guanine" do
     assert RnaTranscription.to_rna(~c"C") == ~c"G"
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes thymidine to adenine" do
     assert RnaTranscription.to_rna(~c"T") == ~c"A"
   end
 
-  @tag :pending
+  # @tag :pending
   test "transcribes adenine to uracil" do
     assert RnaTranscription.to_rna(~c"A") == ~c"U"
   end
 
-  @tag :pending
+  # @tag :pending
   test "it transcribes all dna nucleotides to rna equivalents" do
     assert RnaTranscription.to_rna(~c"ACGTGGTCTTAA") == ~c"UGCACCAGAAUU"
   end
