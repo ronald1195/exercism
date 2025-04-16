f4cae50be8c41bc2445179f6957ce33fa65ebbad
Ronald Munoz <ronald1195@icloud.com>
2025-04-15 22:11:39 -0600

    solution

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -8,5 +8,23 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.map(&rotate_char(&1, shift))
+    |> List.to_string()
+  end
+
+  defp rotate_char(char, shift) when char in ?a..?z do
+    rem(char - ?a + shift, 26) + ?a
+  end
+
+  # This function is used to rotate capital letters
+  # If a space is encountered it is returned as is
+  defp rotate_char(char, shift) when char in ?A..?Z do
+    rem(char - ?A + shift, 26) + ?A
+  end
+
+  defp rotate_char(char, _shift) do
+    char
   end
 end
